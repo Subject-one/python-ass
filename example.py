2b5a0da72150da630c8cb418e76f4e30c5d45d47
Subject-one <clashez@live.com>
2021-07-12 12:18:26 +1000

    setup proper file structure

--- example.py
+++
@@ -1,9 +0,0 @@
-# import cowsay
-# cowsay.cow("You're an excellent coder!")
-# print(cowsay.get_output_string("trex", "I love hugs but my arms are too short"))
-
-import emoji
-import os
-
-os.system("clear")
-print(emoji.emojize("You nailed it :thumbs_up:"))
