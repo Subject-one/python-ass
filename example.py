c66473216e1d88891a8a4f98b057e2ac5f98ec74
Subject-one <clashez@live.com>
2021-07-12 12:09:08 +1000

    Setting up github

---
+++ example.py
@@ -0,0 +1,9 @@
+# import cowsay
+# cowsay.cow("You're an excellent coder!")
+# print(cowsay.get_output_string("trex", "I love hugs but my arms are too short"))
+
+import emoji
+import os
+
+os.system("clear")
+print(emoji.emojize("You nailed it :thumbs_up:"))
